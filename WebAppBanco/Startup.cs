62d6c5a6b324623e469f005e09cac5b28273083f
luisenciso <totomx20@gmail.com>
2017-09-29 18:54:54 -0500

    nb,bnbmnbmnbn

--- WebAppBanco/Startup.cs
+++ WebAppBanco/Startup.cs
@@ -22,6 +22,9 @@ namespace WebAppBanco
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
